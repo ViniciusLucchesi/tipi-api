73b9887e013ea2145727c24116afbb5991fbc20d
ViniciusLucchesi <viniciuslucchesi.ti@gmail.com>
2023-02-27 19:15:37 -0300

    Aplicando OOP

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Robyn(__file__)
 # API routes
 @app.get('/api/ncm/all')
 def sync_ncm_all():
-    context = load_pickle()
+    context = ManageData().load_data()
     resp = json.loads(context.to_json(orient='records', force_ascii=False))
     return json.dumps(resp, indent=4)
 
@@ -16,10 +16,11 @@ def sync_ncm_all():
 @app.get('/api/ncm/:search')
 def sync_ncm_search(request):
     search = request['params']['search']
-    context = load_pickle()
-    search = search_ncm(context, search)
-    resp = json.loads(search.to_json(orient='records', force_ascii=False))
+    context = ManageData().load_data()
+    search = ManageData().search_ncm(context, search)
+    resp = json.loads(search.to_json(orient='records', force_ascii=False))    
     return json.dumps(resp, indent=4)
+    
 
 
 
--- scrapy.py
+++ scrapy.py
@@ -1,125 +1,111 @@
 import re
-import time
 import httpx
 import pandas as pd
-from functools import wraps
 from selectolax.parser import HTMLParser
 from dataclasses import dataclass, asdict
 
 
 @dataclass
-class InfoTIPI:
+class TipiExtension:
     link:str
     extension:str
+
+
+class HTMLBase:
+    def __init__(self) -> None:
+        self.url = 'https://www.gov.br/receitafederal/pt-br/acesso-a-informacao/legislacao/legislacao-por-assunto/tipi-tabela-de-incidencia-do-imposto-sobre-produtos-industrializados'
     
-@dataclass
-class rowTIPI:
-    ncm:str
-    ex:str
-    desc:str
-    aliquota:str
+    def get_html_text(self) -> HTMLParser|int:
+        """
+        Returns HTMLParser if HTTPX.get() request is 200, 
+            otherwise returns an integer type status code
+        """
+        resp = httpx.get(self.url)
+        if resp.status_code == 200:
+            return HTMLParser(resp.text)
+        return resp.status_code
 
-# DECORATORS TO TEST VELOCITY
-def runtime(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        start = time.perf_counter()
-        result = func(*args, **kwargs)
-        end = time.perf_counter()
-        print(result)
-        print(f'{func.__name__} took {end - start:.6f} seconds to complete')
-        return result
-    return wrapper
+    def get_html_content(self, url):
+        resp = httpx.get(url)
+        if resp.status_code == 200:
+            return resp.content
+        return resp.status_code
 
 
-# Scrapy functions
-def get_html() -> HTMLParser:
-    url = 'https://www.gov.br/receitafederal/pt-br/acesso-a-informacao/legislacao/legislacao-por-assunto/tipi-tabela-de-incidencia-do-imposto-sobre-produtos-industrializados'
-    resp = httpx.get(url)
-    return HTMLParser(resp.text)
+class GovernmentScraping(HTMLBase):
+    def __init__(self) -> None:
+        super().__init__()
+        self.html = self.get_html_text()
 
-def get_last_updated() -> str:
-    html = get_html()
-    return html.css_first('span.documentModified').css_first('span.value').text()
-
-def get_link_to_file(html:HTMLParser, extension:str) -> dict:
-    file = html.css_first('p.callout').css(f'a[href$=".{extension}"]')
-    data = InfoTIPI(
-        link=file[0].attrs['href'],
-        extension=file[0].text()
-    )
-    return asdict(data)
-
-def get_all_link_to_files(html:HTMLParser) -> list:
-    files = html.css_first('p.callout').css('a')
-    result = []
-    for type in files:
-        data = InfoTIPI(
-            link=type.attrs['href'],
-            extension=type.text()
+    def get_last_update(self) -> str:
+        return self.html.css_first('span.documentModified').css_first('span.value').text()
+    
+    def get_link_to_file(self, extension:str) -> dict:
+        file = self.html.css_first('p.callout').css(f'a[href$=".{extension.upper()}"]')
+        data = TipiExtension(
+            link=file[0].attrs['href'],
+            extension=file[0].text()
         )
-        result.append(asdict(data))
-    return result
+        return asdict(data)
 
 
-# PICKLE functions
-# @runtime
-def download_tipi(url:str) -> None:
-    request = httpx.get(url).content
-    try:
-        df = pd.read_excel(request, header=7)
-        df = wipe_data(df)
-        df.to_pickle('TIPI.pickle')
-    except Exception as error:
-        print('ERRO: ', error)
-
-# @runtime
-def load_pickle() -> pd.DataFrame:
-    return pd.read_pickle('TIPI.pickle')
-
-
-# Panda functions
-def wipe_data(df:pd.DataFrame) -> pd.DataFrame:
-    # Headers
-    df.columns = df.columns.map(lambda x: x.replace(' ', ''))
-    df = df.replace('(-)|(:)', '', regex=True)
+class ManageData(GovernmentScraping):
+    def __init__(self) -> None:
+        super().__init__()
     
-    # Content
-    df['DESCRIÇÃO'] = df['DESCRIÇÃO'].str.strip()
+    def download_tipi_table(self):
+        url_xlsx = 'https://www.gov.br/receitafederal/pt-br/acesso-a-informacao/legislacao/documentos-e-arquivos/tipi-em-excel.xlsx'
+        request = self.get_html_content(url_xlsx)
+        try:
+            df = pd.read_excel(request, header=7)
+            df = self.wipe_data(df)
+            df.to_pickle('TIPI.pickle')
+            return None
+        except Exception as error:
+            return error
     
-    invalid_ncm = df[df['NCM'].isnull()].index
-    aliquotas_nt = df[df['ALÍQUOTA(%)'] == 'NT'].index
+    def wipe_data(self, df:pd.DataFrame) -> pd.DataFrame:
+        # Headers
+        df.columns = df.columns.map(lambda x: x.replace(' ', ''))
+        df = df.replace('(-)|(:)', '', regex=True)
 
-    for ncm in invalid_ncm:
-        df.loc[ncm, 'NCM'] = df.loc[(ncm - 1), 'NCM']
-
-    for aliquota in aliquotas_nt:
-        df.loc[aliquota, 'ALÍQUOTA(%)'] = 0
-
-    # Pretty
-    resp = df.sort_index().reset_index()
-    resp = resp.drop('index', axis=1)
-    return resp
-
-def search_ncm(df:pd.DataFrame, search:str) -> pd.DataFrame:
-    regex = re.sub('(\.)', '\.', search)
-    regex = f'^({regex})$'
-    retorno = df[df['NCM'].str.contains(regex)]
-
-    if retorno['ALÍQUOTA(%)'].isna:
-        clean = re.sub('(\.)', '', search)
-        regex = re.sub('(\.)', '\.?', search)
-        regex = f'^0?({clean}|{regex}).*$'
-
-        resp = df[df['NCM'].str.contains(regex)]
-        resp = resp.sort_index().reset_index()
+        # Content
+        df['DESCRIÇÃO'] = df['DESCRIÇÃO'].str.strip()
+        invalid_ncm = df[df['NCM'].isnull()].index
+        aliquotas_nt = df[df['ALÍQUOTA(%)'] == 'NT'].index
+        for ncm in invalid_ncm:
+            df.loc[ncm, 'NCM'] = df.loc[(ncm - 1), 'NCM']
+        for aliquota in aliquotas_nt:
+            df.loc[aliquota, 'ALÍQUOTA(%)'] = 0
+        
+        # Pretty
+        resp = df.sort_index().reset_index()
         resp = resp.drop('index', axis=1)
         return resp
-    
-    return retorno
+
+    def load_data(self) -> pd.DataFrame:
+        return pd.read_pickle('TIPI.pickle')
+
+    def search_ncm(self, df:pd.DataFrame, search:str) -> pd.DataFrame:
+        regex = re.sub('(\.)', '\.', search)
+        regex = f'^({regex})$'
+        retorno = df[df['NCM'].str.contains(regex)]
+
+        if retorno['ALÍQUOTA(%)'].isna:
+            clean = re.sub('(\.)', '', search)
+            regex = re.sub('(\.)', '\.?', search)
+            regex = f'^0?({clean}|{regex}).*$'
+
+            resp = df[df['NCM'].str.contains(regex)]
+            resp = resp.sort_index().reset_index()
+            resp = resp.drop('index', axis=1)
+            return resp
+        
+        return retorno
+
 
 
 if __name__ == '__main__':
-    html = get_html()
-    url_xlsx = get_link_to_file(html, 'XLSX')
-    download_tipi(url_xlsx.get('link'))
+    url = 'https://www.gov.br/receitafederal/pt-br/acesso-a-informacao/legislacao/documentos-e-arquivos/tipi-em-excel.xlsx'
+    resp = HTMLBase().get_html_content(url)
+    print(type(resp.content))
